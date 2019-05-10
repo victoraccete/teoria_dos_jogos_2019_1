0b73f82cf1db3889543b9f7862bc4383f6730b86
Victor Accete <victor.accete@gmail.com>
2019-05-10 00:04:40 -0300

    Definindo fluxo do jogo.

--- jogodavelha.py
+++ jogodavelha.py
@@ -8,10 +8,10 @@ def user_moves_first():
         return False
 
 def create_table_matrix():
-    table = np.empty(shape=(3,1), dtype=str)
+    table = np.empty(shape=(3,3), dtype=str)
     return table
 
-def column_and_line_picker():
+def row_and_column_picker():
     valid = False
     while(valid == False):
         user_input = input("Digite: (linha) (coluna)")
@@ -26,8 +26,14 @@ def column_and_line_picker():
             valid = True
     return (line-1, column-1) # -1 por conta dos índices da lista
 
-def mark_table(player, line, column, table):
-    table[line][column] = player
+def user_turn(table):
+    line_and_column = row_and_column_picker()
+    table = mark_table('U', line_and_column[0], line_and_column[1], table)
+    print(table)
+    return table
+
+def mark_table(player, row, column, table):
+    table[row][column] = player
     return table
 
 def is_empty_string(string):
@@ -45,16 +51,37 @@ def choose_first_empty(table):
     print("Não há mais espaços disponíveis no tabuleiro")
     return table
 
-def check_stop_conditions(table):
+def computer_turn(table):
+    table = choose_first_empty(table)
+    print(table)
+    return table
+
+def game_flow(first_time, previous_player, table):
+    if(first_time == True):
+        if(user_moves_first() == True):
+            table = user_turn(table)
+            table = game_flow(False, 'U', table)
+        else:
+            table = computer_turn(table)
+            table = game_flow(False, 'C', table)
+    else:
+        if(previous_player == 'C'):
+            print("Sua vez!")
+            table = user_turn(table)
+            table = game_flow(False, 'U', table)
+        elif(previous_player == 'U'):
+            print("Vez da máquina!")
+            table = computer_turn(table)
+            table = game_flow(False, 'C', table)
+        else:
+            print("Ops, algo estranho aconteceu.")
+    return table
+    
+def check_win_conditions(table):
+    
     return table
 
 table = create_table_matrix()
 print(table)
 print(is_empty_string(table[0][0]))
-line_and_column = column_and_line_picker()
-table = mark_table('U', line_and_column[0], line_and_column[1], table)
-print(table)
-table = choose_first_empty(table)
-print(table)
-table = mark_table('U', line_and_column[2], line_and_column[2], table)
-print(table)
+game_flow(True, None, table)
